b9a9f0217a49e4a657c03de5c37c8d748e8382a7
Zuxelator <zuxel87@mail.ru>
2021-07-22 16:26:27 +0400

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,27 +4,31 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
+import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        for (int i = 1; i <= 5; i++) {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
-        Elements table = doc.select(".forumTable");
-        Elements rows = table.select("tr");
-            for (Element row : rows) {
-                Elements tds = row.select("td");
-                if (tds.size() > 0) {
-                    System.out.println(tds.get(1).child(0).attr("href"));
-                    System.out.println(row.child(1).text());
-                    System.out.println(row.child(5).text());
-                }
-            }
-        }
+public class SqlRuParse implements Parse {
+
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
+
+    public static void main(String[] args) throws Exception {
+        SqlRuParse sqlRuParse = new SqlRuParse(new SqlRuDateTimeParser());
+        List<Post> list = sqlRuParse.list("https://www.sql.ru/forum/job-offers/");
+    }
+
     public static Map<String, String> getDetails(String url) throws IOException {
         Map<String, String> map = new HashMap<>();
         Document doc = Jsoup.connect(url).get();
@@ -32,11 +36,49 @@ public class SqlRuParse {
         Element table = tables.first();
         Elements rows = table.select("tr");
         String description = rows.get(1).child(1).text();
-        String[] split = rows.get(2).child(0).text().split(" \\[");
-        String created = split[0];
+        Elements date = doc.select(".msgFooter");
+        String created = date.get(0).text().split(" \\[")[0];
+        String title = rows.get(0).child(0).text();
         map.put("description", description);
         map.put("created", created);
+        map.put("title", title);
         return map;
     }
 
+    @Override
+    public List<Post> list(String link) {
+        List<Post> rsl = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            Document doc = null;
+            try {
+                doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Elements table = doc.select(".forumTable");
+            Elements rows = table.select("tr");
+            for (Element row : rows) {
+                Elements tds = row.select("td");
+                if (tds.size() > 0) {
+                    rsl.add(detail(tds.get(1).child(0).attr("href")));
+                }
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Post detail(String link) {
+        Post post = null;
+        try {
+            Map<String, String> details = getDetails(link);
+            LocalDateTime created = dateTimeParser.parse(details.get("created"));
+            String title = details.get("title");
+            String description = details.get("description");
+            post = new Post(title, link, description, created);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
 }
